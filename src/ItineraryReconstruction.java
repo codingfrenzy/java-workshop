1e9e7118aecff6cc5f7a7a2463b6af4151d6af5d
codingfrenzy <prstsn@gmail.com>
2016-11-10 16:17:07 -0500

    Itinerary Recronstruction initial

---
+++ src/ItineraryReconstruction.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+public class LongestPalindrome extends ASolution {
+		@Override
+    void runSolution() {
+
+    }
+
+    public String reconstruct(String[][] str){
+    	/*
+    	hashmap destinations
+    	push jfk
+    	while stack not empty
+    		count = 0;
+    		while count <= str.length && noOption()
+    			count++;
+    			current = stack.top()
+    			if (!(current + destinations.get(current)).substr(route))
+    				route += destinations.get(current)
+    				
+    	*/
+    	return null;
+    }
+}
